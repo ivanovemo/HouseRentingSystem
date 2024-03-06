c318ed56a3ffcbdad29e675039d0538f916e2411
Emil Ivanov <emil.ivanov@clever-soft.com>
2024-03-06 09:21:04 +0200

    House Service and House Index model added

---
+++ HouseRentingSystem.Core/Services/House/HouseService.cs
@@ -0,0 +1,32 @@
+﻿using HouseRentingSystem.Core.Contracts.House;
+using HouseRentingSystem.Core.Models.Home;
+using HouseRentingSystem.Infrastructure.Data.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace HouseRentingSystem.Core.Services.House
+{
+    public class HouseService : IHouseService
+    {
+        private readonly IRepository _repository;
+
+        public HouseService(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<IEnumerable<HouseIndexServiceModel>> LastThreeHouses()
+        {
+            return await _repository
+                .AllReadOnly<Infrastructure.Data.Models.House>()
+                .OrderByDescending(h => h.Id)
+                .Take(3)
+                .Select(h => new HouseIndexServiceModel()
+                {
+                    Id = 3,
+                    ImageUrl = h.ImageUrl,
+                    Title = h.Title
+                })
+                .ToListAsync();
+        }
+    }
+}
